ce93c5a8c672d0d3b1e22ee74db1101fb3bad9b9
“Vishal <vsaroopchand@hotmail.com>
2015-08-21 15:41:12 -0400

    + substring impl (own impl)

---
+++ Algorithms/Strings/StringSearch.cs
@@ -0,0 +1,52 @@
+﻿namespace Algorithms.Strings
+{    
+    public static class StringExtensions
+    {
+        public static bool Has(this string source, string pattern)
+        {
+            if (string.IsNullOrEmpty(pattern) || string.IsNullOrEmpty(source))
+            {
+                return false;
+            }
+
+            if (pattern.Length > source.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (i + pattern.Length > source.Length)
+                {
+                    return false;
+                }
+
+                if (source[i] == pattern[0])
+                {
+                    if (pattern.Length == 1)
+                    {
+                        return true;
+                    }
+
+                    var isSubString = true;
+                    for (var j = 1; j < pattern.Length; j++)
+                    {
+                        if (!isSubString)
+                        {
+                            break;
+                        }
+
+                        isSubString = source[i + j] == pattern[j];
+                    }
+
+                    if (isSubString)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;  
+        }    
+    }  
+}
---
+++ MainProgram/AlgorithmsTests/StringSearchTest.cs
@@ -0,0 +1,48 @@
+﻿namespace C_Sharp_Algorithms.AlgorithmsTests
+{
+    using System;
+    using System.Diagnostics;
+
+    using NUnit.Framework;
+    using Algorithms.Strings;
+
+    [TestFixture]
+    public class StringSearchTest
+    {
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            
+        }
+
+        [Test]
+        public void FindSubString()
+        {
+            var source = "The quick brown fox jumps over the lazy dog";
+            var pattern = "n f";
+                        
+            // prime the extension method ~5000 ticks
+            Assert.IsTrue(source.Has("n f"));
+
+            var swMine = Stopwatch.StartNew();
+            Assert.IsTrue(source.Has(" "));
+            swMine.Stop();
+
+            Assert.IsTrue(source.Has("The quick brown fox jumps over the lazy dog"));
+            Assert.IsTrue(source.Has("dog"));
+            Assert.IsTrue(source.Has("over the "));
+            Assert.IsTrue(source.Has("f"));            
+            Assert.IsFalse(source.Has("b f"));
+            Assert.IsFalse(source.Has("b f"));
+            Assert.IsFalse(source.Has(null));           
+            
+            var swTheirs = Stopwatch.StartNew();
+            Assert.IsTrue(source.Contains(pattern));
+            swTheirs.Stop();
+
+
+            var diff = swMine.ElapsedTicks - swTheirs.ElapsedTicks;
+            Console.WriteLine(diff);
+        }
+    }
+}
